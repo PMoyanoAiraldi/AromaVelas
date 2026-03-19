114b516a1532cd028bf059d09b5357e4bdd29c3d
PMoyanoAiraldi <pmoyano497@gmail.com>
2026-03-19 18:26:15 -0300

    Mejorando archivo file-upload.service.ts para subida de archivos

--- aroma-velas/src/file-upload/file-upload.service.ts
+++ aroma-velas/src/file-upload/file-upload.service.ts
@@ -1,11 +1,8 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import { FileUploadDto } from './dto/file-upload.dto';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CloudinaryService } from './cloudinary.service';
-import { ProductService } from 'src/product/product.service';
-import {  LineaService } from 'src/linea/linea.service';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Products } from 'src/product/product.entity';
 import { Repository } from 'typeorm';
+import { Products } from 'src/products/products.entity';
 
 
 
@@ -13,24 +10,18 @@ import { Repository } from 'typeorm';
 export class FileUploadService {
     constructor(
         private readonly cloudinaryService: CloudinaryService,
-        private readonly productService: ProductService,
-        private readonly lineaService: LineaService,
         @InjectRepository(Products) 
         private readonly productsRepository: Repository<Products>,
     ){}
 
     async uploadFile(
         file: Express.Multer.File, 
-        entityType: 'product' | 'linea',
+        entityType: 'product',
         entityId?: string
     ): Promise<{ imgUrl: string }>{
     
         if (!file || !file.buffer || !file.originalname) {
             throw new Error('El archivo proporcionado no es válido');
-        }
-
-        if (!['product', 'linea'].includes(entityType)) {
-            throw new Error('El tipo de entidad proporcionado no es válido');
         }
 
         // Determinamos la carpeta según el tipo de entidad
@@ -47,9 +38,9 @@ export class FileUploadService {
 
         // Actualizar la URL de la imagen en la entidad correspondiente usando los servicios
         switch (entityType) {
-            case 'product':
+            case 'product':{
                 if (!entityId) {
-                    throw new Error('No se proporcionó un ID del producto para actualizar.');
+                    throw new BadRequestException('No se proporcionó un ID del producto');
                 }
 
                 //  Actualizar directamente en el repositorio
@@ -58,33 +49,24 @@ export class FileUploadService {
                 });
 
             if (!product) {
-                throw new Error('Producto no encontrado');
+                throw new NotFoundException('Producto no encontrado');;
             }
             
            // Eliminar la imagen anterior si existe
-                if (product.imgUrl && product.imgUrl !== 'default-image-url.jpg') {
+                if (product.img && product.img !== 'default-image-url.jpg') {
                     try {
-                        await this.cloudinaryService.deleteFile(product.imgUrl);
+                        await this.cloudinaryService.deleteFile(product.img);
                     } catch (error) {
                         console.error('Error al eliminar imagen anterior:', error);
                     }
                 }
 
                 // Actualizar solo la URL de la imagen
-                product.imgUrl = url;
+                product.img = url;
                 await this.productsRepository.save(product);
                 console.log("Imagen del producto actualizada", product);
                 break;
-
-        case 'linea':
-            // Valida si `entityId` está presente
-                if (!entityId) {
-                    throw new Error('No se proporcionó un ID de la linea para actualizar.');
-                }
-
-            await this.lineaService.update(entityId, { image: url });
-            break;
-
+            }
         default:
             throw new Error('Tipo de entidad no compatible');
         }
@@ -97,7 +79,7 @@ export class FileUploadService {
         try {
             await this.cloudinaryService.deleteFile(publicId);  // Utilizando el servicio de Cloudinary
         } catch (error) {
-            console.error('Error al eliminar el archivo de Cloudinary:', error);
+            console.error('Error:', error instanceof Error ? error.message : error);
             throw new InternalServerErrorException('Error al eliminar el archivo de Cloudinary');
         }
     }
@@ -106,10 +88,8 @@ export class FileUploadService {
         switch (entityType) {
             case 'product':
                 return 'product';
-            case 'linea':
-                return 'linea';
             default:
-                throw new Error('Tipo de entidad no compatible');
+                throw new BadRequestException('Tipo de entidad no compatible');
         }
     }
 }
--- aroma-velas/src/products/products.service.ts
+++ aroma-velas/src/products/products.service.ts
@@ -14,105 +14,105 @@ export class ProductsService {
         private readonly categoryService: CategoryService
     ){}
 
-    // async createProduct(createProductDto: CreateProductDto, file?: Express.Multer.File): Promise<ResponseProductDto> {
-    //     try {
-    //         console.log('Datos del DTO recibidos:', createProductDto);
+    async createProduct(createProductDto: CreateProductDto, file?: Express.Multer.File): Promise<ResponseProductDto> {
+        try {
+            console.log('Datos del DTO recibidos:', createProductDto);
             
-    //         const normalizedName = createProductDto.name.trim().toUpperCase();
+            const normalizedName = createProductDto.name.trim().toUpperCase();
     
-    //         const productExist = await this.productsRepository
-    //             .createQueryBuilder('product')
-    //             .where('UPPER(product.name) = :name', { name: normalizedName })
-    //             .getOne();
+            const productExist = await this.productsRepository
+                .createQueryBuilder('product')
+                .where('UPPER(product.name) = :name', { name: normalizedName })
+                .getOne();
     
-    //         if (productExist) {
-    //             throw new HttpException(
-    //                 `El producto con el nombre "${createProductDto.name}" ya existe.`,
-    //                 HttpStatus.BAD_REQUEST,
-    //             );
-    //         }
+            if (productExist) {
+                throw new HttpException(
+                    `El producto con el nombre "${createProductDto.name}" ya existe.`,
+                    HttpStatus.BAD_REQUEST,
+                );
+            }
     
-    //         const category = await this.categoryService.findOne(createProductDto.lineaId);
-    //         if (!linea) {
-    //             throw new NotFoundException(`Linea con ID ${createProductDto.lineaId} no encontrada`);
-    //         }
-    //         console.log('Linea encontrada:', linea);
+            const category = await this.categoryService.findOne(createProductDto.lineaId);
+            if (!linea) {
+                throw new NotFoundException(`Linea con ID ${createProductDto.lineaId} no encontrada`);
+            }
+            console.log('Linea encontrada:', linea);
 
-    //         if (createProductDto.marcaId) {
-    //         const marca = await this.marcaService.findOne(createProductDto.marcaId);
-    //         if (!marca) {
-    //             throw new NotFoundException(`Marca con ID ${createProductDto.marcaId} no encontrada`);
-    //         }
-    //         console.log('Marca encontrada:', marca);
-    //     }
+            if (createProductDto.marcaId) {
+            const marca = await this.marcaService.findOne(createProductDto.marcaId);
+            if (!marca) {
+                throw new NotFoundException(`Marca con ID ${createProductDto.marcaId} no encontrada`);
+            }
+            console.log('Marca encontrada:', marca);
+        }
 
-    //         if (createProductDto.rubroId) {
-    //         const rubro = await this.rubroService.findOneRubro(createProductDto.rubroId);
-    //         if (!rubro) {
-    //             throw new NotFoundException(`Rubro con ID ${createProductDto.rubroId} no encontrado`);
-    //         }
-    //         console.log('Rubro encontrado:', rubro);
-    //     }
+            if (createProductDto.rubroId) {
+            const rubro = await this.rubroService.findOneRubro(createProductDto.rubroId);
+            if (!rubro) {
+                throw new NotFoundException(`Rubro con ID ${createProductDto.rubroId} no encontrado`);
+            }
+            console.log('Rubro encontrado:', rubro);
+        }
 
-    //       // Subir la imagen si existe un archivo
-    //         let imageUrl: string | undefined;
-    //         if (file) {
-    //             try {
-    //                 // Espera la carga de la imagen
-    //                 imageUrl = await this.cloudinaryService.uploadFile(file.buffer, 'product', file.originalname);
-    //                 console.log('Archivo subido a Cloudinary:', imageUrl);
-    //             } catch (error) {
-    //                 console.error('Error al subir la imagen a Cloudinary:', error);
-    //                 throw new InternalServerErrorException('Error al subir la imagen');
-    //             }
-    //         }
+          // Subir la imagen si existe un archivo
+            let imageUrl: string | undefined;
+            if (file) {
+                try {
+                    // Espera la carga de la imagen
+                    imageUrl = await this.cloudinaryService.uploadFile(file.buffer, 'product', file.originalname);
+                    console.log('Archivo subido a Cloudinary:', imageUrl);
+                } catch (error) {
+                    console.error('Error al subir la imagen a Cloudinary:', error);
+                    throw new InternalServerErrorException('Error al subir la imagen');
+                }
+            }
 
-    //           // Crear el producto sin el precio
-    //         const newProduct = this.productsRepository.create({
-    //             nombre: normalizedName,
-    //             descripcion: createProductDto.descripcion,
-    //             codigo: createProductDto.codigo,
-    //             codigoAlternativo1: createProductDto.codigoAlternativo1,
-    //             codigoAlternativo2: createProductDto.codigoAlternativo2,
-    //             marca: { id: createProductDto.marcaId }, 
-    //             linea: createProductDto.lineaId ? { id: createProductDto.lineaId } : undefined, 
-    //             rubro: { id: createProductDto.rubroId }, 
-    //             //subRubro: createProductDto.subrubroId ? { id: createProductDto.subrubroId } : undefined, 
-    //             imgUrl: imageUrl || createProductDto.imgUrl || 'default-image-url.jpg',
-    //             //precios: precio ? [precio] : [] // Asociar el precio si existe
-    //         });
+              // Crear el producto sin el precio
+            const newProduct = this.productsRepository.create({
+                nombre: normalizedName,
+                descripcion: createProductDto.descripcion,
+                codigo: createProductDto.codigo,
+                codigoAlternativo1: createProductDto.codigoAlternativo1,
+                codigoAlternativo2: createProductDto.codigoAlternativo2,
+                marca: { id: createProductDto.marcaId }, 
+                linea: createProductDto.lineaId ? { id: createProductDto.lineaId } : undefined, 
+                rubro: { id: createProductDto.rubroId }, 
+                //subRubro: createProductDto.subrubroId ? { id: createProductDto.subrubroId } : undefined, 
+                imgUrl: imageUrl || createProductDto.imgUrl || 'default-image-url.jpg',
+                //precios: precio ? [precio] : [] // Asociar el precio si existe
+            });
 
-    //         // Guardar la clase y esperar su confirmación
-    //         const savedProduct = await this.productsRepository.save(newProduct);
-    //         console.log("Producto guardado", savedProduct)
+            // Guardar la clase y esperar su confirmación
+            const savedProduct = await this.productsRepository.save(newProduct);
+            console.log("Producto guardado", savedProduct)
     
-    //         //Validamos y obtenemos el precio si se proporciona
-    //         if (createProductDto.precio) {
-    //             const precio = this.precioRepository.create({
-    //             producto: savedProduct, // Asociar con el producto recién creado
-    //             precio: createProductDto.precio,
-    //             listaPrecio: createProductDto.listaPrecio || 1 // Lista 1 por defecto
-    //         });
-    //         await this.precioRepository.save(precio);
-    //         console.log('Precio guardado:', precio);
-    //         }
+            //Validamos y obtenemos el precio si se proporciona
+            if (createProductDto.precio) {
+                const precio = this.precioRepository.create({
+                producto: savedProduct, // Asociar con el producto recién creado
+                precio: createProductDto.precio,
+                listaPrecio: createProductDto.listaPrecio || 1 // Lista 1 por defecto
+            });
+            await this.precioRepository.save(precio);
+            console.log('Precio guardado:', precio);
+            }
     
-    //         // Cargar las relaciones para el response
-    //     const productWithRelations = await this.productsRepository.findOne({
-    //         where: { id: savedProduct.id },
-    //         relations: ['marca', 'linea', 'rubro', 'precios']
-    //     });
+            // Cargar las relaciones para el response
+        const productWithRelations = await this.productsRepository.findOne({
+            where: { id: savedProduct.id },
+            relations: ['marca', 'linea', 'rubro', 'precios']
+        });
 
-    //         return ResponseProductDto.fromEntity(productWithRelations);
-    //     } catch (error) {
-    //         if (error instanceof QueryFailedError && error.driverError?.code === '23505') {
-    //             throw new HttpException(
-    //                 'Ya existe un producto con ese nombre.',
-    //                 HttpStatus.BAD_REQUEST,
-    //             );
-    //         }
-    //         throw error;
-    //     }
-    // }
+            return ResponseProductDto.fromEntity(productWithRelations);
+        } catch (error) {
+            if (error instanceof QueryFailedError && error.driverError?.code === '23505') {
+                throw new HttpException(
+                    'Ya existe un producto con ese nombre.',
+                    HttpStatus.BAD_REQUEST,
+                );
+            }
+            throw error;
+        }
+    }
 
 }
